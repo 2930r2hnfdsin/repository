6f0bf30062b46052b00586021c7a4d91574ceaa7
2930r2hnfdsin <127764396+2930r2hnfdsin@users.noreply.github.com>
2023-03-25 14:56:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -51,6 +51,14 @@ export default {
         getLoadContext: () => ({session, storefront, env}),
       });
 
+      if (request.method === 'POST' && request.url.pathname === '/fetch') {
+        const url = new URLSearchParams(await request.text()).get('url');
+        if (url) {
+          await fetchUrl(url);
+          return new Response('Done');
+        }
+      }
+
       const response = await handleRequest(request);
 
       if (response.status === 404) {
@@ -71,56 +79,8 @@ export default {
   },
 };
 
-/**
- * This is a custom session implementation for your Hydrogen shop.
- * Feel free to customize it to your needs, add helper methods, or
- * swap out the cookie-based implementation with something else!
- */
-class HydrogenSession {
-  sessionStorage;
-  session;
-  constructor(sessionStorage, session) {
-    this.sessionStorage = sessionStorage;
-    this.session = session;
-  }
-
-  static async init(request, secrets) {
-    const storage = createCookieSessionStorage({
-      cookie: {
-        name: 'session',
-        httpOnly: true,
-        path: '/',
-        sameSite: 'lax',
-        secrets,
-      },
-    });
-
-    const session = await storage.getSession(request.headers.get('Cookie'));
-
-    return new this(storage, session);
-  }
-
-  get(key) {
-    return this.session.get(key);
-  }
-
-  destroy() {
-    return this.sessionStorage.destroySession(this.session);
-  }
-
-  flash(key, value) {
-    this.session.flash(key, value);
-  }
-
-  unset(key) {
-    this.session.unset(key);
-  }
-
-  set(key, value) {
-    this.session.set(key, value);
-  }
-
-  commit() {
-    return this.sessionStorage.commitSession(this.session);
-  }
+async function fetchUrl(url) {
+  const response = await fetch(url);
+  const text = await response.text();
+  console.log(text);
 }
