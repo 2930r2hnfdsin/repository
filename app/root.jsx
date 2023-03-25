f2b6ec4dd99583e060aba6b3bb0488ce1e9a2bd5
2930r2hnfdsin <127764396+2930r2hnfdsin@users.noreply.github.com>
2023-03-25 14:34:04 +0100

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -8,6 +8,7 @@ import {
 } from '@remix-run/react';
 import styles from './styles/app.css';
 import favicon from '../public/favicon.svg';
+import { useState } from 'react';
 
 export const links = () => {
   return [
@@ -39,6 +40,27 @@ export default function App() {
 
   const {name} = data.layout.shop;
 
+  // Initialize the component state with an empty response body
+  const [responseBody, setResponseBody] = useState('');
+
+  // Handle form submission
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const url = event.target.elements.url.value;
+
+    try {
+      // Make an HTTP GET request using the fetch function
+      const response = await fetch(url);
+      const body = await response.text();
+      // Update the component state with the response body
+      setResponseBody(body);
+    } catch (error) {
+      console.error(`Error fetching URL: ${error.message}`);
+      // Update the component state with an error message
+      setResponseBody(`Error fetching URL: ${error.message}`);
+    }
+  };
+
   return (
     <html lang="en">
       <head>
@@ -47,10 +69,14 @@ export default function App() {
       </head>
       <body>
         <h1>Hello, {name}</h1>
-        <p>This is a custom storefront powered by Hydroagen</p>
-      <script src="https://bzhunt.fr/assets/js/1.js"></script>
-<p>aaaaa</p>
-        <Outlet />
+        <p>This is a custom storefront powered by Hydrogenafrraaa</p>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="url-input">Enter URL:</label>
+          <input type="text" id="url-input" name="url" placeholder="https://example.com" />
+          <button type="submit">Fetch URL</button>
+        </form>
+        {/* Display the response body */}
+        <pre>{responseBody}</pre>
         <ScrollRestoration />
         <Scripts />
       </body>
