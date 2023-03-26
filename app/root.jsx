13b0a5274a6f89919466b7cd4e046fcfcf1c0758
2930r2hnfdsin <127764396+2930r2hnfdsin@users.noreply.github.com>
2023-03-26 17:00:47 +0200

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -29,7 +29,7 @@ export const meta = () => ({
   charset: 'utf-8',
   viewport: 'width=device-width,initial-scale=1',
 });
-
+ 
 export async function loader({context}) {
   const layout = await context.storefront.query(LAYOUT_QUERY);
   return {layout};
